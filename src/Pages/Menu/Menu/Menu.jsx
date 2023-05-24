a2b8b5314975a59a039fcc01822724704286868a
rahatul <rahatul99@gmail.com>
2023-05-25 02:59:05 +0600

    menu section header coplited

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,7 +1,19 @@
+import { Helmet } from 'react-helmet-async';
+import Cover from '../../Shared/Cover/Cover';
+import menuImg from '../../../assets/menu/banner3.jpg';
+import PopularMenu from '../../Home/PopularMenu/PopularMenu';
 const Menu = () => {
     return (
         <div>
-            
+            <Helmet>
+                <title>Bistro Boss | Menu</title>
+            </Helmet>
+            <Cover img={menuImg} title="Our menu" />
+            <PopularMenu />
+            <Cover img={menuImg} title="Our menu" />
+            <PopularMenu />
+            <Cover img={menuImg} title="Our menu" />
+            <PopularMenu />
         </div>
     );
 };
