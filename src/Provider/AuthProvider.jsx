727e59c65a57797af77f4c9ce1347782f175714e
rahatul <rahatul99@gmail.com>
2023-05-30 20:51:27 +0600

    jwt and axios used to secure authentication token

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -46,7 +46,7 @@ const AuthProvider = ({children}) => {
             if(currentUser){
                 axios.post('http://localhost:5000/jwt', {email: currentUser.email})
                 .then(data => {
-                    console.log(data.data.token);
+                    // console.log('jwt',data);
 
                     localStorage.setItem('access-token', data.data.token)
                 })
--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,16 +1,53 @@
-import { useQuery } from '@tanstack/react-query'
-import { useContext } from 'react';
-import { AuthContext } from '../Provider/AuthProvider';
-const useCart = () => {
-    const { user } = useContext(AuthContext);
+// import { useQuery } from '@tanstack/react-query'
+// import { useContext } from 'react';
+// import { AuthContext } from '../Provider/AuthProvider';
+// const useCart = () => {
+//     const { user } = useContext(AuthContext);
 
-    const { refetch ,data: cart = [] } = useQuery({
+//     const token = localStorage.getItem('access-token'); //purpose of jwt
+
+//     const { refetch ,data: cart = [] } = useQuery({
+//         queryKey: ['carts', user?.email],
+//         queryFn: async () => {
+//             const response = await fetch(`http://localhost:5000/carts?email=${user?.email}`, {
+//                 headers: {authorization: `bearer ${token}`}
+//             })  //purpose of jwt after the url-{}
+//             return response.json();
+//           },
+//       })
+//       return [cart , refetch]
+// }
+// export default useCart;
+
+
+
+
+
+
+
+
+
+
+
+//------------------------------- Option two----------------------------------------//
+import { useQuery } from '@tanstack/react-query'
+import useAxiosSecure from './useAxiosSecure';
+import useAuth from './useAuth';
+
+const useCart = () => {
+    const { user, loading } = useAuth();
+    const [axiosSecure] = useAxiosSecure();
+    const { refetch, data: cart = [] } = useQuery({
         queryKey: ['carts', user?.email],
+        enabled: !loading,
         queryFn: async () => {
-            const response = await fetch(`http://localhost:5000/carts?email=${user?.email}`)
-            return response.json();
-          },
-      })
-      return [cart , refetch]
+            const res = await axiosSecure(`/carts?email=${user?.email}`)
+            console.log('res from axios', res)
+            return res.data;
+        },
+    })
+
+    return [cart, refetch]
+
 }
 export default useCart;
